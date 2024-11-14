f6b35226bcf57721f7b7cec3df0e182ed68dfbce
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-14 10:47:24 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,17 @@
 function convertToAdjList(adjMatrix) {
-    
+  let adjList = [];
     //Use nested for loops to traverse through matrix
+  for(let i = 0; i < adjMatrix.length; i++) {
+    for(let j = 0; j < adjMatrix.length; j++) {
+      if(matrix[i][j] == 1) {
+        adjList[i].push(j);
+      }
+      else {}
+    }
+  }
     //if 1, there is a connection through, if 0 there is no connection
     //if there is a connection, add to list
     //return finsihed list
     
-    return adjMatrix;
+    return adjList;
 }
